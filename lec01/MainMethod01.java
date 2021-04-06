fe082e58a1a0f6fa57cadccd8b3f5516c28b3ec3
Gihwan <lgh95m@naver.com>
2021-04-06 12:20:06 +0900

    new file

---
+++ lec01/MainMethod01.java
@@ -0,0 +1,14 @@
+package lec01;
+
+public class MainMethod01 {
+
+	public static void main (String[] args) {//자바 메인 메서드의 기본 규격,틀 그냥 이렇게 써야함.
+		dosomething();
+		System.out.println("Hello World!");
+		
+		
+	}
+	public static void dosomething() {
+		System.out.println("docode");
+	}
+}
---
+++ lec01/Veriable02.java
@@ -0,0 +1,18 @@
+package lec01;
+
+public class Veriable02 {
+
+	public static void main(String[] args) {
+		
+		int intAmount = 10;//프로그래밍 작업에서 변수의 초기화는 굉장히 중요한 작업
+		int a=1;
+		int b=2; // 이런식으로 아무런 의미없는 변수명을 선언해주는것은 굉장히 좋지않은 습관임. 항상 의미가 있는 변수명을 선언해야함
+		int firstValue =10;
+		int SecondValue = 2;
+		int sumValue=0;
+		
+		sumValue = firstValue + SecondValue;
+		sumValue = firstValue + SecondValue;
+		System.out.println(sumValue);
+	}
+}
