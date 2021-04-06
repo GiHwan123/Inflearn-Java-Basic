f0a6370e5f008608130902c54802c7a46b90d71d
Gihwan <lgh95m@naver.com>
2021-04-06 19:32:01 +0900

    new file

--- lec02/DoWhile05.java
+++ lec02/DoWhile05.java
@@ -3,11 +3,15 @@ package lec02;
 public class DoWhile05 {
 
 	public static void main (String[] args) {
-		int whileCnt=0;
+		/*
+		 * int whileCnt=0; do { System.out.println("first,do");
+		 * System.out.println("whileCnt :::" + whileCnt); whileCnt++;
+		 * }while(whileCnt<3);
+		 */
+		
 		do {
-			System.out.println("first,do");
-			System.out.println("whileCnt :::" + whileCnt);
-			whileCnt++;
-		}while(whileCnt<3);
+			System.out.println("Do, first");
+		}while(false);
+		
 	}
 }
